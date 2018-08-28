fd69c5945babd0d84030614200ccbc842c6cff9e
guyd@linux <guydvir.tech@gmail.com>
2018-08-28 21:54:16 +0300

    after flasing in ESP8266 OK

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import utime
-
+import machine
 
 def loc_switch():
     import localswitchOnly
@@ -10,11 +10,10 @@ import startWifi_staticIP
 import updateClockESP
 
 utime.sleep(3)
-pin_up = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
-utime.sleep(1)
-if pin_up.value() != 0:
-    loc_switch()
-else:
-    import MQTTcom_lite
+# pin_up = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+# utime.sleep(1)
+# if pin_up.value() != 0:
+#     loc_switch()
+# else:
+import MQTTcom_lite
 
-#
